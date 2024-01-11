7c8a3aecf088a63447f4edff0ba6841c3c6f97de
Jean Tituaña <jeantituana@hotmail.com>
2024-01-10 23:56:18 -0500

    Todo con imagen y fecha

---
+++ Todo/Todo/Models/TodoItem.cs
@@ -0,0 +1,17 @@
+﻿using SQLite;
+
+namespace Todo.Models
+{
+    public class TodoItem
+    {
+        [PrimaryKey, AutoIncrement]
+        public int ID { get; set; }
+        public string Name { get; set; }
+
+        public string Dia { get; set; }
+        public string Notes { get; set; }
+        public bool Done { get; set; }
+
+        public string ImagePath { get; set; } // Nueva propiedad para la ruta de la imagen
+    }
+}
---
+++ Todo/Todo/Views/TodoItemPage.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using Todo.Data;
+using Todo.Models;
+using Xamarin.Forms;
+using Xamarin.Essentials;
+
+namespace Todo.Views
+{
+    public partial class TodoItemPage : ContentPage
+    {
+        public TodoItemPage()
+        {
+            InitializeComponent();
+        }
+
+        async void OnSaveClicked(object sender, EventArgs e)
+        {
+            var todoItem = (TodoItem)BindingContext;
+            TodoItemDatabase database = await TodoItemDatabase.Instance;
+            await database.SaveItemAsync(todoItem);
+            await Navigation.PopAsync();
+        }
+
+        async void OnDeleteClicked(object sender, EventArgs e)
+        {
+            var todoItem = (TodoItem)BindingContext;
+            TodoItemDatabase database = await TodoItemDatabase.Instance;
+            await database.DeleteItemAsync(todoItem);
+            await Navigation.PopAsync();
+        }
+
+        async void OnCancelClicked(object sender, EventArgs e)
+        {
+            await Navigation.PopAsync();
+        }
+
+        async void OnUploadImageClicked(object sender, EventArgs e)
+        {
+            try
+            {
+                var result = await MediaPicker.PickPhotoAsync();
+                if (result != null)
+                {
+                    // Guardar la ruta de la imagen seleccionada en la propiedad ImagePath
+                    ((TodoItem)BindingContext).ImagePath = result.FullPath;
+                    // Mostrar la imagen seleccionada en la vista previa
+                    imagePreview.Source = ImageSource.FromFile(result.FullPath);
+                }
+            }
+            catch (Exception ex)
+            {
+                // Manejar errores al seleccionar la imagen
+                Console.WriteLine($"Error selecting image: {ex.Message}");
+            }
+        }
+
+    }
+}
